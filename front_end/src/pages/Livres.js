614b2463049521d7d6bde4085d806e52ed96904f
Stephane DUFOUR <dufstef@hotmail.fr>
2023-11-02 13:34:25 +0100

    feat ( front end ) livres

--- front_end/src/pages/Livres.js
+++ front_end/src/pages/Livres.js
@@ -36,7 +36,7 @@ const Livres = () => {
                 <div class="px-6 py-4">
                   <div class="font-bold text-xl mb-2">{book.title}</div>
                   <p class="text-gray-700 text-base">
-                    {book.summary}
+                    {book.summary.slice(0,50)}
                   </p>
                   <p>{book.isbn}</p>
                 </div>
