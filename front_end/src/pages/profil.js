3b8974c8210d67f9f96ad1232ea3fdd6f087ccd2
Stephane DUFOUR <dufstef@hotmail.fr>
2023-11-17 11:52:39 +0100

    feat ( front end ) style and controles

--- front_end/src/pages/profil.js
+++ front_end/src/pages/profil.js
@@ -52,7 +52,6 @@ const Profil = () => {
 
   return (
     <div className='flex justify-center'>
-      {user && (console.log(user))}
       {user && (
 
         <Formik
@@ -95,16 +94,18 @@ const Profil = () => {
                     name="lastName"
                     type="text"
                     placeholder="votre nom"
+                    onChange={e => setUser({ ...user, lastName: e.target.value })}
                   />
                 </div>
-                <div className={myClassName}>
+                {/* <div className={myClassName}>
                   <MyTextInput
                     label="Votre prénom"
                     name="firstName"
                     type="text"
                     placeholder="votre prénom"
+                    onChange={e => setUser({ ...user, firstName: e.target.value })}
                   />
-                </div>
+                </div> */}
               </div>
 
               <button className='my-2 md:my-1 self-center font-bold p-2 border-2' type="submit">Modifier</button>
